542b9ecddb34bdde46e6d4875e66c58d512cb41e
Andrew L <alavaia@yahoo.com>
2019-01-04 08:30:45 -0500

    initial commit of graph

---
+++ main.py
@@ -0,0 +1,24 @@
+import pdb
+import time
+
+from graphs import Graph
+
+
+def main():
+    graph = Graph()
+    graph.loadMap("map_default.yml")
+    pdb.set_trace()
+
+if __name__ == '__main__':
+    main()
+
+# TODO
+# set up graph where each intersection represents a vertex
+# create road class that defines lanes, intersections, name, speed limit, and collision information
+# create car class that defines id (license plate #?), size, preferred speed, starting point, and ending destination
+# AI so cars can change lanes without crashing and adjust route based on existing traffic conditions
+# Algo to find shortest path/quickest expected path
+# use optparser to initially take command line args
+# create gui menu so that settings can be changed in the simulation (# of cars, lane closures, etc)
+# create a run simulation method 
+# feature to import existing road systems from Google Maps API?
